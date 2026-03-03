94c2d31b141de9af4fb4b6f864e45f511e76e8d0
Cole Boehlert <crboehlert@gmail.com>
2026-03-03 10:12:38 -0700

    the draw man file
    this draws the guesses along with the winning and losing screens. this also checks if the guess is equal to the target word

---
+++ drawMan.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int x;
+	int y;
+
+	char word[] = "program";
+	char guess[50]; 
+
+	printf(" ------\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("______||\n");
+
+	printf("Enter a guess: ");
+	scanf("%49s", &guess);
+do {
+
+	if (strcmp(word, guess) == 0) {
+		y++;
+	}
+
+	else {
+		x++;
+	}
+
+	if (x == 1) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 2) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf("  |   ||\n");
+        	printf("  |   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 3) {
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|   ||\n");
+        	printf("  |   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 4) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|)  ||\n");
+        	printf("  |   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 5) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|)  ||\n");
+        	printf("  |   ||\n");
+        	printf(" (    ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 6) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|)  ||\n");
+        	printf("  |   ||\n");
+        	printf(" ( )  ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		printf("GAME OVER. YOU LOSE.\n");
+	
+		break;
+	}
+
+	if (y == 1) {
+
+		printf("GAME OVER. YOU WIN.\n");
+
+		break;
+	}
+	
+	else {
+		printf("Something bad happned");
+	}
+
+	printf("Enter a guess: ");
+        scanf("%49s", &guess);
+
+}while(1);
+
+	return 0;
+}
