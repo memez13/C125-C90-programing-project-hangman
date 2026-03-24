5d917662087c0f158a7daee647cc39b1e5da3493
Cole Boehlert <crboehlert@gmail.com>
2026-03-24 09:22:28 -0700

    Add files via upload

---
+++ test.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "wordCheck.c"
+#define MAX_TRIES 7;
+#define WORDS 5;
+#define WORD_LEN 25;
+
+const char *word_bank[] = { "extraterrestrial", "industry", "vote", "map", "contemporary", "program" };
+
+int checkGuess(char word[], char display[], char guess) {  //check guess letter to random word from word_bank
+	int found = 0;
+	int i;
+	for (i = 0; i < strlen(word); i++) {
+			if (word[i] == guess){
+				display[i] = guess;
+				found = 1;
+			}
+	}
+	return found;
+}
+
+void showStatus(char display[], int triesLeft){		//rough draft pulled from homework. Make it implement hangman display instead
+		printf("\nCurrent Progress: ");
+		int i;
+		for(i = 0; i < strlen(display); i++){
+				printf("%c", display[i]);
+		}
+		printf("\n");
+}
+
+int main() {   //Somehow figure out a way to implement memory allocation into this project.....
+	int x;
+	int y;
+	int c;
+
+	char word[] = "program";
+	char guess[50]; 
+
+	printf(" ------\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("      ||\n");
+	printf("______||\n");
+
+	printf("I have selected a word.\n");
+    	wordLength();
+    	printf("\n");
+    	printf("Make a guess: ");
+    	scanf("%49s", &guess);
+	
+do {
+
+	if (strcmp(word, guess) == 0) {
+		y++;
+	}
+
+	else {
+		x++;
+		printf("Wrong guess!\n");
+		wordLength();
+	}
+
+	if (x == 1) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 2) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf("  |   ||\n");
+        	printf("  |   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 3) {
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|   ||\n");
+        	printf("  |   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 4) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|)  ||\n");
+        	printf("  |   ||\n");
+        	printf("      ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 5) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|)  ||\n");
+        	printf("  |   ||\n");
+        	printf(" (    ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		
+	}
+
+	else if (x == 6) {
+
+		printf(" ------\n");
+        	printf("  |   ||\n");
+        	printf("  O   ||\n");
+        	printf(" (|)  ||\n");
+        	printf("  |   ||\n");
+        	printf(" ( )  ||\n");
+        	printf("      ||\n");
+        	printf("______||\n");
+
+		printf("GAME OVER! YOU LOSE!\n");
+	
+		break;
+	}
+
+	if (y == 1) {
+
+		printf("GAME OVER. YOU WIN.\n");
+
+		break;
+	}
+	
+	else {
+		//to give the code an out
+	}
+
+	printf("Make a guess: ");
+        scanf("%49s", &guess);
+
+}while(1);
+
+	return 0;
+}
