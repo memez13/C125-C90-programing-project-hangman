02ff959c0b4726a154f973084d99451ff986b7ac
Cole Boehlert <crboehlert@gmail.com>
2026-03-27 16:06:34 -0700

    Update wordCheck.c

--- wordCheck.c
+++ wordCheck.c
@@ -3,8 +3,6 @@
 
 
 void wordLength() {
-        int len;
-        int i;
 
         const char *word_bank[] = { "extraterrestrial", "industry", "vote", "map", "contemporary" };
         int x = sizeof(word_bank) / sizeof(word_bank[0]);
@@ -14,13 +12,6 @@ void wordLength() {
         int random_index = rand() % x;
 
         const char *random_word = word_bank[random_index];
-        //implement random word pulled from word_bank (might need to pull it from another FILE)
 
-        len = sizeof(random_word) / sizeof(random_word[0]);
-
-        for (i = 0; i < len; i++) {
-                printf("_ ");
-        }
-        printf("\n");
-}
+        
 
