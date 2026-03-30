5a26f89e8e4adcf618bf7e8edb68e180f4f6620d
snon-ops <stanleydrapesaiii@gmail.com>
2026-03-29 23:28:06 -0700

    Update wordCheck1.c

--- wordCheck1.c
+++ wordCheck1.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-
-void wordSelect() {
-
-        const char *word_bank[] = { "extraterrestrial", "industry", "vote", "map", "contemporary" };
-        int x = sizeof(word_bank) / sizeof(word_bank[0]);
-
-        srand((unsigned)time(NULL));
-
-        int random_index = rand() % x;
-
-        const char *random_word = word_bank[random_index];
-}
+void wordLength(char *revealed) {
+	int i;
+	int len = strlen(revealed);
+	for (i = 0; i < len; i++) {
+    	            printf("%c", revealed[i]);
+        	}
+        	printf("\n");
+	}
         
 int checkGuess(const char *word, const char *guess, char *revealed) {  //check guess letter to random word from word_bank
 	int found = 0;
